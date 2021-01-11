93aff714e38daf6a4625fdcbdcf9a981b117d94e
uptoy <u24to45y@gmail.com>
2021-01-11 21:58:02 +0900

    [Add]Aside

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,9 +1,35 @@
 import React from 'react'
-import { Container } from './styles'
+import {MdDashboard , MdArrowDownward ,MdArrowUpward ,MdExitToApp} from 'react-icons/md'
+import logoImg from '../../assets/logo.svg'
+import { Container,Header,LogImg,Title,MenuContainer ,MenuItemLink} from  './styles'
 
 const Aside:React.FC = () => {
     return (
-        <h1>aside</h1>
+        <Container>
+            <Header>
+                <LogImg src={logoImg} alt="Logo" />
+            </Header>
+            <MenuContainer>
+                <MenuItemLink href="#">
+                    <MdDashboard />
+                    DashBoard
+                    </MenuItemLink>
+
+                <MenuItemLink href="#">
+                    <MdArrowUpward />
+                    111
+                </MenuItemLink>
+
+                <MenuItemLink href="#">
+                    <MdArrowDownward />
+                    222
+                </MenuItemLink>
+
+                <MenuItemLink href='#'>
+                    <MdExitToApp />
+                </MenuItemLink>
+            </MenuContainer>
+        </Container>
     )
 }
 
